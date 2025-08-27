efee181fff9d207a1754c0aee11b89c714e03786
Martha Maloba <marthamarumo92@gmail.com>
2025-08-27 16:10:31 +0200

    Create Colors.tsx

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,8 @@
+const Colors = {
+  primary: '#1E90FF',     // Dodger Blue
+  secondary: '#FF6347',   // Tomato
+  background: '#FFFFFF',  // White
+  text: '#333333',        // Dark Gray
+};
+
+export default Colors;
