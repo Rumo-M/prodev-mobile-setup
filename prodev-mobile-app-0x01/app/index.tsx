aadb41c4ba2570462c569addf2cc7b6f59433bd2
Martha Maloba <marthamarumo92@gmail.com>
2025-08-27 16:24:53 +0200

    Modify index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,17 +4,11 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Main Entry Text */}
       <Text>Entry Screen - Awesome</Text>
 
-      {/* Additional Text Components */}
       <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
