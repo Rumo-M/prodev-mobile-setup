4d2848495f979b269fa455fb3fa97ab4b5b3e2db
Martha Maloba <marthamarumo92@gmail.com>
2025-08-29 18:38:49 +0200

    Update index.tsx

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,58 +1,59 @@
 import React from "react";
-import {
-  Text,
-  View,
-  Image,
-  ImageBackground,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
 import styles from "./styles";
 
-export default function Index() {
+export default function LoginScreen() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <ImageBackground
-          source={require("@/assets/images/background-image.png")}
-          style={styles.background}
-          resizeMode="cover"
-        >
-          <View style={styles.container}>
-            <Text style={styles.entryText}>Entry Screen - Awesome</Text>
+    <View style={styles.container}>
+      <Text style={styles.largeText}>Welcome Back!</Text>
+      <Text style={styles.placeholderText}>
+        Enter your email and password to sign in.
+      </Text>
 
-            <View style={styles.companyLogo}>
-              <Image source={require("@/assets/images/Logo.png")} />
-            </View>
+      <View style={styles.formGroup}>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Email"
+          keyboardType="email-address"
+        />
+      </View>
 
-            <View style={styles.textGroup}>
-              <Text style={styles.textLarge}>Find your favorite place here</Text>
-              <Text style={styles.textSmall}>The best prices for over 2 </Text>
-              <Text style={styles.textSmall}>million properties worldwide</Text>
-            </View>
+      <View style={styles.passwordGroup}>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Password"
+          secureTextEntry
+        />
+      </View>
 
-            <Text style={styles.textLarge}>Typescript is great if you practice more</Text>
-            <Text style={styles.textSmall}>React Native provides you a single codebase for cross platforms</Text>
-            <Text style={styles.textSmall}>ALX is awesome</Text>
+      <TouchableOpacity>
+        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </TouchableOpacity>
 
-            <View style={styles.buttonGroup}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
-              </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.largeText}>Sign In</Text>
+      </TouchableOpacity>
 
-              <TouchableOpacity style={styles.transparentButton}>
-                <Text style={styles.textSmall}>Sign In</Text>
-              </TouchableOpacity>
-            </View>
+      <Text style={styles.placeholderText}>or continue with</Text>
 
-            <View style={styles.continue}>
-              <Text style={{ color: "white" }}>Continue to home</Text>
-            </View>
-          </View>
-        </ImageBackground>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      <View style={styles.navGroup}>
+        <TouchableOpacity style={styles.socialMediaButton}>
+          <Image
+            source={require("@/assets/images/google.png")}
+            style={{ width: 24, height: 24, marginRight: 10 }}
+          />
+          <Text>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.socialMediaButton}>
+          <Image
+            source={require("@/assets/images/facebook.png")}
+            style={{ width: 24, height: 24, marginRight: 10 }}
+          />
+          <Text>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
