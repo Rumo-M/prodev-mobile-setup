b1f28058069201c9009d8ca1fd3dbf410744e01d
Martha Maloba <marthamarumo92@gmail.com>
2025-08-29 18:08:59 +0200

    Create styles/index.tsx

---
+++ prodev-mobile-app-0x03/styles/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  textLarge: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  textSmall: {
+    fontSize: 14,
+    color: "#666",
+  },
+  textGroup: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+});
+
+export default styles;
