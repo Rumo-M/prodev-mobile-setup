c69c0552f0ef137c83a6406425edbea68897e19b
Martha Maloba <marthamarumo92@gmail.com>
2025-08-31 23:48:38 +0200

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,7 +7,6 @@ export default function App() {
       <Text style={styles.largeText}>Typescript is great if you practice more</Text>
       <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
-      <Text style={styles.entryText}>Entry Screen - Awesome</Text>
     </View>
   );
 }
@@ -18,12 +17,6 @@ const styles = StyleSheet.create({
     padding: 16,
     justifyContent: "center",
     alignItems: "center",
-  },
-  entryText: {
-    fontSize: 26,
-    fontWeight: "bold",
-    marginVertical: 12,
-    textAlign: "center",
   },
   largeText: {
     fontSize: 24,
